db4dc37d83b1f5b94c4c3a17652699781f9af3ca
Myka Tolentino <z3546537@ad.unsw.edu.au>
2025-10-28 02:02:06 -0500

    Updated readme and driver manager

--- src/main/java/com/nsw/revenue/drivers/ChromeDriverManager.java
+++ src/main/java/com/nsw/revenue/drivers/ChromeDriverManager.java
@@ -7,27 +7,17 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
 
-public class ChromeDriverManager implements DriverManager {
+public class ChromeDriverManager extends DriverManager {
     @Override
     public RemoteWebDriver createDriver() throws MalformedURLException {
         var browserOptions = new ChromeOptions();
         browserOptions.setPlatformName("Windows 10");
         browserOptions.setBrowserVersion("latest");
 
-        HashMap<String, Object> ltOptions = new HashMap<>();
-        ltOptions.put("project", "LambdaTest Revenue NSW Automation");
-        ltOptions.put("build", "LambdaTest V1.0.0");
-        ltOptions.put("name", "Stamp Duty Calculator");
-        ltOptions.put("w3c", true);
-        ltOptions.put("plugin", "java-testNG");
-
-        browserOptions.setCapability("LT:Options", ltOptions);
-
-        String gridUrl = "@hub.lambdatest.com/wd/hub";
+        browserOptions.setCapability("LT:Options", getLtOptions());
         return new RemoteWebDriver(
-                new URL("http://" + Base64Util.decryptString(ConfigUtil.getProperty("username")) + ":" + Base64Util.decryptString(ConfigUtil.getProperty("access.key")) + gridUrl),
+                new URL("http://" + Base64Util.decryptString(ConfigUtil.getProperty("username")) + ":" + Base64Util.decryptString(ConfigUtil.getProperty("access.key")) + ConfigUtil.getProperty("grid.url")),
                 browserOptions
         );
     }
--- src/main/java/com/nsw/revenue/drivers/DriverManager.java
+++ src/main/java/com/nsw/revenue/drivers/DriverManager.java
@@ -4,7 +4,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
+import java.util.HashMap;
 
-public interface DriverManager {
-    RemoteWebDriver createDriver() throws MalformedURLException;
+public abstract class DriverManager {
+    abstract RemoteWebDriver createDriver() throws MalformedURLException;
+
+    public HashMap<String, Object> getLtOptions() {
+        HashMap<String, Object> ltOptions = new HashMap<>();
+        ltOptions.put("project", "LambdaTest Revenue NSW Automation");
+        ltOptions.put("build", "LambdaTest V1.0.0");
+        ltOptions.put("name", "Stamp Duty Calculator");
+        ltOptions.put("w3c", true);
+        ltOptions.put("plugin", "java-testNG");
+        return ltOptions;
+    }
 }
